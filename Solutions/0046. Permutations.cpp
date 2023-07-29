ac2a00b222e044ce40af83382e4107ee839b0aa2
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-07-29 22:39:49 +0530

    Update 0046. Permutations.cpp

--- Solutions/0046. Permutations.cpp
+++ Solutions/0046. Permutations.cpp
@@ -1,24 +1,53 @@
 class Solution {
-private:
-    void Helper(vector<int> nums, vector<vector<int>>& ans, int index){
-        //Base case
+public:
+    void Helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp, int freq[]){
+        if(temp.size() >= nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int i=0;i<nums.size();i++){
+            if(!freq[i]){
+                temp.push_back(nums[i]);
+                freq[i] = 1;
+                Helper(nums, ans, temp, freq);
+                freq[i] = 0;
+                temp.pop_back();
+            }
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        int freq[nums.size()];
+        for(int i=0;i<nums.size();i++){
+            freq[i] = 0;
+        }
+        Helper(nums, ans, temp, freq);
+        return ans;
+    }
+
+    /*
+    void Helper(vector<int>& nums, vector<vector<int>>& ans, int index){
         if(index >= nums.size()){
             ans.push_back(nums);
             return;
         }
 
-        for(int j=index;j<nums.size();j++){
-            swap(nums[index], nums[j]);
+        for(int i = index;i<nums.size();i++){
+            swap(nums[index], nums[i]);
             Helper(nums, ans, index+1);
-            //Backtraking
-            swap(nums[index], nums[j]);
+            swap(nums[i], nums[index]);
         }
     }
-public:
+
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         int index = 0;
         Helper(nums, ans, index);
         return ans;
     }
+    */
 };
+
