7a145c2afe0e120e8186e58a324b21dfbddf0291
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-07-29 22:55:18 +0530

    Update 0102. Binary Tree Level Order Traversal.cpp

--- Solutions/0102. Binary Tree Level Order Traversal.cpp
+++ Solutions/0102. Binary Tree Level Order Traversal.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    /*
     vector<vector<int>> Traversal;
 
     void Helper(TreeNode* root, int depth){
@@ -31,5 +32,36 @@ public:
         Helper(root, 0);
         return Traversal;
 
+    }
+    */
+
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == nullptr){
+            return ans;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> temp;
+            for(int i=0;i<size;i++){
+                TreeNode *front = q.front();
+                q.pop();
+
+                temp.push_back(front->val);
+                if(front->left != nullptr){
+                    q.push(front->left);
+                }
+
+                if(front->right != nullptr){
+                    q.push(front->right);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
 };
