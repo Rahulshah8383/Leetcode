3ea1f4477090afc5b7a9a697f197f08932d73ac3
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-08-04 00:45:05 +0530

    Create 0864. Shortest Path to Get All Keys.cpp

---
+++ Solutions/0864. Shortest Path to Get All Keys.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int n,m,keys;
+    int dx[4] = {1,-1,0,0};
+    int dy[4] ={0,0,1,-1};
+
+    int shortestPathAllKeys(vector<string>& grid) {
+        queue<pair<pair<int,int>,int>>q;
+        n=grid.size();
+        m=grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='@')q.push({{i,j},0});
+                if(grid[i][j]>='a' && grid[i][j]<='z')keys++;
+            }
+        }
+        int check = (1<<keys)-1;
+        map<pair<pair<int,int>,int>,int>p;
+        int ans=0;
+        while(q.size()){
+            int k=q.size();
+            for(int i=0;i<k;i++){
+                auto j = q.front();q.pop();
+                int x = j.first.first;
+                int y = j.first.second;
+                int mask = j.second;
+                if(mask==check)return ans;
+                for(int h=0;h<4;h++){
+                    int nx = x + dx[h];
+                    int ny = y + dy[h];
+                    if(nx>=0 && ny>=0 && ny<m && nx<n && grid[nx][ny]!='#'){
+                        if(grid[nx][ny]>='a' && grid[nx][ny]<='z'){
+                            int n_mask = mask | (1<<(grid[nx][ny]-'a'));
+                            if(p.find({{nx,ny},n_mask})==p.end()){
+                                p[{{nx,ny},n_mask}]=n_mask;
+                                q.push({{nx,ny},n_mask});
+                            }
+                        }
+                        else if(grid[nx][ny]>='A' && grid[nx][ny]<='Z'){
+                            int c = grid[nx][ny]-'A';
+                            if((mask & (1<<c)) == 0)continue;
+                            int n_mask = mask;
+                            if(p.find({{nx,ny},n_mask})==p.end()){
+                               p[{{nx,ny},n_mask}]=n_mask;
+                                q.push({{nx,ny},n_mask});
+                            }
+                        }
+                        else{
+                            int n_mask = mask;
+                            if(p.find({{nx,ny},n_mask})==p.end()){
+                                p[{{nx,ny},n_mask}]=n_mask;
+                                q.push({{nx,ny},n_mask});
+                            }
+                        }
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+};
