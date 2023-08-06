db81ba5d1e825481868a7d7752bab5cc434d33d7
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-08-06 23:44:30 +0530

    Create 1575. Count All Possible Routes.cpp

---
+++ Solutions/1575. Count All Possible Routes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    const int MOD = 1000000007;
+    int Helper(const vector<int>& locations, int city, int finish, int remainFuel, vector<vector<int>>& memo) {
+        if (remainFuel < 0) {
+            return 0;
+        }
+        
+        if (memo[city][remainFuel] != -1) {
+            return memo[city][remainFuel];
+        }
+        
+        int total = city == finish ? 1 : 0;
+        
+        for (int nextCity = 0; nextCity < locations.size(); nextCity++) {
+            if (nextCity != city && remainFuel >= abs(locations[nextCity] - locations[city])) {
+                total = (total + Helper(locations, nextCity, finish, remainFuel - abs(locations[nextCity] - locations[city]), memo)) % MOD;
+            }
+        }
+        
+        return memo[city][remainFuel] = total;
+    }
+
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        int n = locations.size();
+        vector<vector<int>> memo(n, vector<int>(fuel + 1, -1));
+        return Helper(locations, start, finish, fuel, memo);
+    }
+};
