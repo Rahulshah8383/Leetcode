34381ad59c6440f544743bcce302e167064bb1be
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-08-08 11:28:47 +0530

    Update 0074. Search a 2D Matrix.cpp

--- Solutions/0074. Search a 2D Matrix.cpp
+++ Solutions/0074. Search a 2D Matrix.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        /*
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        if(m==0 || n == 0){
+            return false;
+        }
+
+        int start = matrix[0][0];
+        int end = matrix[m-1][n-1];
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            int row = 0;
+            int col = n-1;
+
+            while(row < m && col >=0){
+                if(matrix[row][col] == target)
+                    return true;
+                else if(matrix[row][col] < target)
+                    row++;
+                else
+                    col--; 
+            }
+
+            if(row == m)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return false;
+        */
+        
         int row = matrix.size();
         int col = matrix[0].size();
         
