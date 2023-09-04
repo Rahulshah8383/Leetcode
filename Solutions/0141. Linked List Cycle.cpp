801d4cf0175dd2e69d9e4e0007d3ca0d5de0c0e3
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-09-04 22:06:44 +0530

    Update 0141. Linked List Cycle.cpp

--- Solutions/0141. Linked List Cycle.cpp
+++ Solutions/0141. Linked List Cycle.cpp
@@ -9,24 +9,37 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL){
+        // if(head == NULL || head->next == NULL){
+        //     return false;
+        // }
+
+        // ListNode *slow = head;
+        // ListNode *fast = head;
+
+        // while(slow != NULL && fast != NULL){
+        //     fast = fast->next;
+        //     if(fast != NULL){
+        //         fast = fast->next;
+        //     }
+
+        //     slow = slow->next;
+
+        //     if(slow == fast){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        if(head==NULL)
             return false;
-        }
-
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while(slow != NULL && fast != NULL){
-            fast = fast->next;
-            if(fast != NULL){
-                fast = fast->next;
-            }
-
-            slow = slow->next;
-
-            if(slow == fast){
+            
+        unordered_map<ListNode*,int> map;
+        while(head!=NULL){
+            if(map.count(head)>0)
                 return true;
-            }
+            else
+                map[head]=1;
+            head=head->next;
         }
         return false;
     }
