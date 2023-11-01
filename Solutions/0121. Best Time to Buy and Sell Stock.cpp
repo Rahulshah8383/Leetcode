f4b1f970a7b1ef33c66156d7d4ae8e206e013c9f
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-11-02 01:21:43 +0530

    Update 0121. Best Time to Buy and Sell Stock.cpp

--- Solutions/0121. Best Time to Buy and Sell Stock.cpp
+++ Solutions/0121. Best Time to Buy and Sell Stock.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       int Buy = prices[0], max_profit = 0; 
+       // int Buy = prices[0], max_profit = 0; 
+
+       // for(int i=0;i<prices.size();i++){
+       //     if(Buy > prices[i]){
+       //         Buy = prices[i];
+       //     }
+
+       //     else if(prices[i]-Buy > max_profit){
+       //         max_profit = prices[i]-Buy;
+       //     }
+       // }
+       // return max_profit;
+
+        int mini = prices[0], max_profit = 0; 
 
        for(int i=0;i<prices.size();i++){
-           if(Buy > prices[i]){
-               Buy = prices[i];
-           }
-
-           else if(prices[i]-Buy > max_profit){
-               max_profit = prices[i]-Buy;
-           }
+           int cost = prices[i] - mini;
+           max_profit = max(max_profit, cost);
+           mini = min(mini, prices[i]);
        }
        return max_profit;
     }
