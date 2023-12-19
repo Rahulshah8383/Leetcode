5533c8b8c48eefc0f2190d1c4e997fc079c43403
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2023-12-19 10:15:12 +0530

    Create 1688. Count of Matches in Tournament.cpp

---
+++ Solutions/1688. Count of Matches in Tournament.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        // int ans = 0;
+
+        // while(n > 1){
+        //     ans += n/2;
+        //     if((n&1) == 0){
+        //         n = n/2;
+        //     }
+        //     else{
+        //         n = 1 + n/2;
+        //     }
+        // }
+
+        // return ans;
+
+        return n-1;
+    }
+};
