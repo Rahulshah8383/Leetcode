4727704df87766f5bdeabf1fb3cce3808da7dec1
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2024-02-02 19:36:51 +0530

    Create 2706. Buy Two Chocolates.cpp

---
+++ Solutions/2706. Buy Two Chocolates.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+
+        int a = prices[0];
+        int b = prices[1];
+
+        if(money >= (a+b)){
+            return money - (a+b);
+        }
+
+        return money;
+    }
+};
