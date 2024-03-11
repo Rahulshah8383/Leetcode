68f534a299e3e7c634d08ea37297638ae1a0b443
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2024-03-11 23:41:14 +0530

    Update 0078. Subsets.cpp

--- Solutions/0078. Subsets.cpp
+++ Solutions/0078. Subsets.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:
+    /*
     void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
         //base case
         if(index >= nums.size()) {
@@ -15,12 +16,31 @@ private:
         output.push_back(element);
         solve(nums, output, index+1, ans);
     }
+   */
 
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        /*
         vector<vector<int>> ans;
         vector<int> output;
         solve(nums, output, 0, ans);
         return ans;
+        */
+
+        int n = nums.size();
+
+        int subset_ct = (1<<n);
+        vector<vector<int>> subsets;
+
+        for(int mask=0;mask<subset_ct;mask++){
+            vector<int> subset;
+            for(int i=0;i<n;i++){
+                if((mask & (1<<i)) != 0){
+                    subset.push_back(nums[i]);
+                }
+            }
+            subsets.push_back(subset);
+        }
+        return subsets;
     }
 };
