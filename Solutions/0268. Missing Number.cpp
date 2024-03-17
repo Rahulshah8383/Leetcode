2ae9dc8fe64e2007ab26eb2971b0df31831ddd20
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2024-03-17 23:18:01 +0530

    Update 0268. Missing Number.cpp

--- Solutions/0268. Missing Number.cpp
+++ Solutions/0268. Missing Number.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int miss = 0;
 
-        for (int i = 0; i < nums.size(); i ++) {
-            miss = miss ^ i + 1;
-            miss = miss ^ nums[i];
+        // o(nlogn)
+        // int ans;
+
+        // sort(nums.begin(), nums.end());
+        // for(int i=0;i<nums.size();i++){
+        //     if(i ==  nums[i]){
+        //         continue;
+        //     }
+        //     else{
+        //         return i;
+        //     }
+        // }
+
+        // return nums.size();   
+
+        // O(n)
+        // int total = nums.size()*(nums.size()+1)/2;
+        // int sum = accumulate(nums.begin(), nums.end(), 0);
+        // return total-sum;
+
+        int ans = nums.size();
+        int i=0;
+
+        for(int num : nums){
+            ans ^= num;
+            ans ^= i;
+            i++;
         }
 
-        return miss;
+        return ans;
     }
 };
