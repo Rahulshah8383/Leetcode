457b3292624ad1c65ab2f03ebe0a92c8c110b700
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2024-03-19 23:30:14 +0530

    Update 0997. Find the Town Judge.cpp

--- Solutions/0997. Find the Town Judge.cpp
+++ Solutions/0997. Find the Town Judge.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> trusting(n+1, 0);
-        vector<int> trusted(n+1, 0);
+        // unordered_map<int, list<int>> adjList;
+        // for(int i=0;i<trust.size();i++){
+        //     adjList[trust[i][0]].push_back(trust[i][1]);
+        // }
+
+        // int ans = -1;
+        // for(auto  it : adjList){
+        //     // cout<<it.first<<" : ";
+        //     // for(auto i : it.second){
+        //     //     cout<<i<<" ";
+        //     // }
+        //     // cout<<endl;
+
+        //     if(it.second.size() == 0){
+        //         ans = it.first;
+        //     }
+        // }
+
+        vector<int> outdegree(n+1, 0);
+        vector<int> indegre(n+1, 0);
 
         for(int i=0;i<trust.size();i++){
-            trusting[trust[i][0]]++;
-            trusted[trust[i][1]]++;
-        }
+            int u = trust[i][0];
+            int v = trust[i][1];
 
+            outdegree[u]++;
+            indegre[v]++;
+        }
+        
         int ans = -1;
+
         for(int i=1;i<=n;i++){
-            if(trusting[i] == 0 && trusted[i] == n-1){
-                ans = i;
+            // cout<<i<<" : "<<outdegree[i]<<" "<<indegre[i]<<endl;
+
+            if(outdegree[i] == 0 && indegre[i] == n-1){
+                ans =i;
             }
         }
 
