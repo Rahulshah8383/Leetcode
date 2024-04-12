f97e162bfbd652a9c12aa199a013b3769d33bdde
Rahul Shah <109208059+Rahulshah8383@users.noreply.github.com>
2024-04-12 22:57:03 +0530

    Create 0442. Find All Duplicates in an Array.cpp

---
+++ Solutions/0442. Find All Duplicates in an Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i]) != mp.end()){
+                ans.push_back(nums[i]);
+            }
+            else{
+                mp[nums[i]]++;
+            }
+        }
+
+        return ans;
+    }
+};
